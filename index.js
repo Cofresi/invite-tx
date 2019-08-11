61f5ef6f221f7e2c0b661eddb8d5b68b800db495
Cofresi <cofresi@dash.org>
2019-08-10 23:29:19 -0400

    use normal tx with sighash none

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const {
   Transaction,
   PrivateKey,
-  PublicKey,
-  Address,
   crypto,
 } = require('@dashevo/dashcore-lib');
 const commander = require('commander');
@@ -23,70 +21,77 @@ async function logOutput(msg, delay = 50) {
  */
 async function create() {
 
-  let faucetPrivateKey;
-  let faucetAddress;
+  let bobAddress;
   let bobPrivateKey;
-  let bobUserName;
+  let alicePrivateKey;
 
-  const privKey = 'cVwyvFt95dzwEqYCLd8pv9CzktajP4tWH2w9RQNPeHYA7pH35wcJ';
-  faucetPrivateKey = new PrivateKey(privKey);
+  const alicePrivKey = 'cRK3oHMLoayzAz2UUheFRRymtAeejte4yb4jf33PxZaj172HcaJ3';
+  alicePrivateKey = new PrivateKey(alicePrivKey);
 
-  const faucetPublicKey = PublicKey.fromPrivateKey(faucetPrivateKey);
-
-  faucetAddress = Address
-    .fromPublicKey(faucetPublicKey, 'testnet')
-    .toString();
-
-  await logOutput(`faucetAddress ${faucetAddress}`);
-
-  //bobUserName = 'bob_' + Math.random().toString(36).substring(7);
-  bobUserName = 'bob';
-  await logOutput(`bobUserName ${bobUserName}`);
+  bobAddress ='yeXXy72V7hWXnPoy82qZQ5XsqZtFfuWH6G';
+  await logOutput(`bobAddress ${bobAddress}`);
 
   bobPrivateKey = new PrivateKey();
   await logOutput(`bobPrivateKey ${bobPrivateKey}`);
-
-  const validPayload = new Transaction.Payload.SubTxRegisterPayload()
-    .setUserName(bobUserName)
-    .setPubKeyIdFromPrivateKey(bobPrivateKey).sign(bobPrivateKey);
-
-  await logOutput(`validPayload ${validPayload}`);
 
   //const inputs = await dapiClient.getUTXO(faucetAddress);
   let inputs = {};
   inputs
     .items = [
       {
-      address:"yereyozxENB9jbhqpbg1coE5c39ExqLSaG",
-      txid:"6303c60d2735c7080e4faf993bba33c90aa6b4f45ac5832873b1b127bb23dc69",
-      vout:0,
-      scriptPubKey:"76a914cb594917ad4e5849688ec63f29a0f7f3badb5da688ac",
-      amount:11.25000415,
-      satoshis:1125000415,
-      height:149553,
-      confirmations:1
+        address:"yeXXy72V7hWXnPoy82qZQ5XsqZtFfuWH6G",
+        txid:"70fd6da8544ffe0a083f5e9a50ae361755dae223f10e179c627d99ad5f23fe29",
+        vout:1,
+        scriptPubKey:"76a914c7bb875d7fd38c672ce10427e7e172735c827f0388ac",
+        amount:11.250023,
+        satoshis:1125002300,
       },
       {
-        address:"yereyozxENB9jbhqpbg1coE5c39ExqLSaG",
-        txid:"ee5c088affd44f3e91a35257bd1c93fd469b39c805c60aa5d7febac6948350d7",
-        vout:0,
-        scriptPubKey:"76a914cb594917ad4e5849688ec63f29a0f7f3badb5da688ac",
+        address:"yeXXy72V7hWXnPoy82qZQ5XsqZtFfuWH6G",
+        txid:"f0d31cd8f6a644aa3766227953c1d4f5da62184994e7016113f597cfef905ab4",
+        vout:1,
+        scriptPubKey:"76a914c7bb875d7fd38c672ce10427e7e172735c827f0388ac",
         amount:11.25,
         satoshis:1125000000,
-        height:149552,
-        confirmations:2
       },
     ];
 
   const transaction = new Transaction()
-    .setType(Transaction.TYPES.TRANSACTION_SUBTX_REGISTER)
-    .setExtraPayload(validPayload)
     .from(inputs.items)
-    .addFundingOutput(10000)
-    .change(faucetAddress)
-    .sign(faucetPrivateKey, crypto.Signature.SIGHASH_NONE);
+    .change(bobAddress)
+    .sign(alicePrivateKey, crypto.Signature.SIGHASH_NONE);
 
-  await logOutput(`invite tx ${transaction}`);
+  const inviteTx = transaction.serialize();
+  await logOutput(`invite tx ${inviteTx}`);
+
+  // just export the P2PKH signature script
+
+  // create P2PKH
+  const input1 = Transaction.Input.fromObject(transaction.inputs[0]);
+  const input2 = Transaction.Input.fromObject(transaction.inputs[1]);
+
+  const pkh1 = new Transaction.Input.PublicKeyHash(input1);
+  const pkh2 = new Transaction.Input.PublicKeyHash(input2);
+
+  await logOutput(`script 1 ${input1.toObject().script}`);
+  await logOutput(`script 2 ${input2.toObject().script}`);
+
+  await logOutput(`script 1 isFullySigned ${pkh1.isFullySigned()}`);
+  await logOutput(`script 2 isFullySigned ${pkh2.isFullySigned()}`);
+
+  const userTx = new Transaction();
+  // add signed inputs
+  userTx.inputs.push(pkh1);
+  userTx.inputs.push(pkh2);
+  // add bob's address as only output
+  const availableInviteAmount = userTx._getInputAmount() - userTx._estimateFee();
+  userTx.to(bobAddress, availableInviteAmount);
+
+  await logOutput(`available amount ${userTx._getInputAmount()}`);
+  await logOutput(`fee ${userTx._estimateFee()}`);
+  await logOutput(`actual amount ${availableInviteAmount}`);
+  await logOutput(`userTx ${userTx}`);
+  await logOutput(`isFullySigned ${userTx.isFullySigned()}`);
 }
 
 commander
